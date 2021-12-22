6b9d3ce341837ab73ca2cfef0380d5bda90ab12f
tim <tim@tim-VirtualBox>
2021-12-22 20:40:04 +0400

    Пустое окно и настроенный Мавен

--- cal/src/main/java/com/poortoys/examples/AppMain.java
+++ cal/src/main/java/com/poortoys/examples/AppMain.java
@@ -1,9 +1,20 @@
 package com.poortoys.examples;
 
-public class AppMain {
+import javax.swing.JFrame;
 
+public class AppMain{
+	private JFrame frame;
+	static AppMain okno=new AppMain();
+	public AppMain() {
+		frame=new JFrame();
+        frame.setTitle ("Калькулятор стоимости строительства частного дома");
+        frame.setBounds(300,0,600,700);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        frame.setVisible(true);
+	}
     public static void main(String[] args) {
-        System.out.println("Hello, world");
     }
 
 }
